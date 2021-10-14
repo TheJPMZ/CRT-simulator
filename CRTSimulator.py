8ed36353b658ff23aba00027b3ac92bb33ea7889
TheJPMZ <64183934+TheJPMZ@users.noreply.github.com>
2021-10-14 01:44:33 -0600

    Changed some names for english support and optimizations

--- CRTSimulator.py
+++ CRTSimulator.py
@@ -2,8 +2,6 @@ import math
 import turtle
 from tkinter import *
 
-#TODO: Documentation
-
 root = Tk() 
 root.config(bd=15)
 
@@ -28,8 +26,14 @@ current_mode = 1
 def changemode():
     global current_mode,Omega1,Omega2
     if current_mode == 0:
+         
         placas_verticales.set(100)
         placas_horizontales.set(100)
+        
+        label_3["state"] = "disable"
+        label_4["state"] = "disable"
+        label_5["state"] = "normal"
+        label_6["state"] = "normal"
         
         placas_horizontales["state"] = "disable"
         placas_verticales["state"] = "disable"
@@ -39,11 +43,17 @@ def changemode():
         R3["state"] = "normal"
         R4["state"] = "normal"
         
+        
         current_mode = 1
         
     else: 
         desfase.set(0)
         
+        label_3["state"] = "normal"
+        label_4["state"] = "normal"
+        label_5["state"] = "disable"
+        label_6["state"] = "disable"
+    
         placas_horizontales["state"] = "normal"
         placas_verticales["state"] = "normal"
         desfase["state"] = "disable"
@@ -57,36 +67,42 @@ def changemode():
        
         current_mode = 0
 
-Label(root, text="Voltaje de Aceleración").pack()
+label_1 = Label(root, text="Acceleration Voltage")
+label_1.pack()
 
 scale = Scale(root, from_=0, to=255,orient=HORIZONTAL)
 scale.set(127)
 scale.pack()
 
-Label(root, text="Tiempo de latencia").pack()
+label_2 = Label(root, text="\nLatency")
+label_2.pack()
 
 latency = Scale(root, from_=1, to=100,orient=HORIZONTAL)
 latency.set(50)
 latency.pack()
 
-Label(root, text="Voltaje de placas Verticales").pack()
+label_3 = Label(root, text="\n\nVertical plate voltage")
+label_3.pack()
 
 placas_verticales = Scale(root, from_=-100, to=100,orient=HORIZONTAL)
 placas_verticales.pack()
 placas_verticales.set(0)
 
-Label(root, text="Voltaje de placas Horizontales").pack()
+label_4 = Label(root, text="\nHorizontal plate voltage")
+label_4.pack()
 
 placas_horizontales = Scale(root, from_=-100, to=100,orient=HORIZONTAL)
 placas_horizontales.pack()
 placas_horizontales.set(0)
 
-Label(root, text="Desfase entre ambos oscilladores").pack()
+label_5 = Label(root, text="\n\nPhase shift between both oscillators")
+label_5.pack()
 
 desfase = Scale(root, from_=0, to=180,orient=HORIZONTAL)
 desfase.pack()
 
-Label(root, text="Frecuencia").pack()
+label_6 = Label(root, text="\nFrequency")
+label_6.pack()
 
 var = StringVar()
 
@@ -109,21 +125,23 @@ R3.pack( anchor = CENTER,side=TOP)
 R4 = Radiobutton(root,justify=CENTER, text="2:3", variable=var, value="2,3",command=changeOmega)
 R4.pack( anchor = CENTER,side=TOP)
 
-Button(root, text = "Cambio de Modo",command=changemode).pack(side=LEFT)
+Button(root, text = "Change mode",command=changemode).pack(side=LEFT)
 
-Button(root, text = "Limpiar",command=canvasclear).pack(side=LEFT)
+Button(root, text = "Clean Screen",command=canvasclear).pack(side=LEFT)
 
 onLoop = True
 def salir():
     global onLoop
     onLoop = False
 
-Button(root, text = "Salir",command=salir).pack(side=LEFT)
+Button(root, text = "Leave",command=salir).pack(side=LEFT)
 
-changemode() 
+changemode()    
 
-turtle.Screen().bgcolor("Black")
+
+turtle.screensize(canvwidth=400,canvheight= 400,bg = "Black")
 turtle.Screen().colormode(255)
+
 
 x_value = turtle.Turtle()
 y_value = turtle.Turtle()
@@ -140,7 +158,8 @@ x_value.penup()
 y_value.penup()
 screen_value.penup()
 
-screen_value.turtlesize(0.5)       
+screen_value.turtlesize(0.5)
+screen_value.pensize(5)       
 
 def draw(time):
 
